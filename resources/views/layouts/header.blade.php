b94e89e3533aae4d4f7c2bd16ca76641b55b3482
faisalaltaf <altafffaisal60@gmail.com>
2021-04-05 15:31:50 +0500

    first

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Dashboard </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="/product">Products </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/customer">customer</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/setting">setting</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+    
+    </span>
+  </div>
+  
+</nav>
---
+++ resources/views/navbar/setting.blade.php
@@ -0,0 +1,24 @@
+@extends('shopify-app::layouts.default')
+
+@section('content')
+   setting
+   
+@endsection
+@section('scripts')
+    @parent
+
+<!-- <h2>forward_static_call_array</h2> -->
+    <script type="text/javascript">
+        var AppBridge = window['app-bridge'];
+        var actions = AppBridge.actions;
+        var TitleBar = actions.TitleBar;
+        var Button = actions.Button;
+        var Redirect = actions.Redirect;
+        var titleBarOptions = {
+            title: 'setting',
+        };
+        var myTitleBar = TitleBar.create(app, titleBarOptions);
+    </script>
+
+@endsection
+
